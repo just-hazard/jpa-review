89a96b42b94c3709c288c25258a3bd03c9a0a2a1
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-09-05 15:15:34 +0900

    cascade type

--- src/main/java/just/hazard/jpa_review/config/JpaRunner.java
+++ src/main/java/just/hazard/jpa_review/config/JpaRunner.java
@@ -1,6 +1,8 @@
 package just.hazard.jpa_review.config;
 
 import just.hazard.jpa_review.domain.Account;
+import just.hazard.jpa_review.domain.Comment;
+import just.hazard.jpa_review.domain.Post;
 import just.hazard.jpa_review.domain.Study;
 import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
@@ -20,18 +22,36 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Account account = new Account();
-        account.setPassword("hazard");
-        account.setUsername("eden");
+//        Account account = new Account();
+//        account.setPassword("hazard");
+//        account.setUsername("eden");
+//
+//        Study study = new Study();
+//        study.setName("JPA");
+//
+//        account.getStudies().add(study);
+//        study.setOwner(account);
+//
+//        Session session = entityManager.unwrap(Session.class);
+//        session.save(account);
+//        session.save(study);
+//
+//        Account load = session.load(Account.class, account.getId());
+//        System.out.println("==========================");
+//        System.out.println(load.getUsername());
 
-        Study study = new Study();
-        study.setName("JPA");
+        Post post = new Post();
+        post.setTitle("title");
 
-        account.getStudies().add(study);
-        study.setOwner(account);
+        Comment comment = new Comment();
+        comment.setComment("comment1");
+        post.setComments(comment);
+
+        Comment comment1 = new Comment();
+        comment1.setComment("comment2");
+        post.setComments(comment1);
 
         Session session = entityManager.unwrap(Session.class);
-        session.save(account);
-        session.save(study);
+        session.save(post);
     }
 }
