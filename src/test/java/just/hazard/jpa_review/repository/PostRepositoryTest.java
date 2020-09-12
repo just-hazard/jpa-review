2dac3bcbe78295968d39c71cdcd8a990ccb6d5cd
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-09-12 15:29:08 +0900

    spring data common null process

--- src/test/java/just/hazard/jpa_review/repository/PostRepositoryTest.java
+++ src/test/java/just/hazard/jpa_review/repository/PostRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +23,8 @@ class PostRepositoryTest {
 
         Post save = postRepository.save(post);
         assertThat(save.getId()).isNotNull();
+
+        Optional<Post> byId = postRepository.findById(100l);
+        assertThat(byId).isEmpty();
     }
 }
